4c5a22fe1536959a03a6478c3b200e8ada74f7b2
Kagiri11 <charlesmaish423@gmail.com>
2021-08-20 02:48:50 +0300

    implemented character details

--- app/src/main/java/com/example/harrypotter/ui/adapters/CharacterAdapter.kt
+++ app/src/main/java/com/example/harrypotter/ui/adapters/CharacterAdapter.kt
@@ -1,5 +1,33 @@
 package com.example.harrypotter.ui.adapters
 
-class CharacterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.harrypotter.R
+import com.example.harrypotter.databinding.ItemCharacterBinding
+import com.example.harrypotter.models.Character
 
+class CharacterAdapter(val characters: List<Character>) :
+    RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
+    class CharacterViewHolder(val binding: ItemCharacterBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ItemCharacterBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.item_character, parent, false)
+        return CharacterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        val character = characters[position]
+        holder.binding.apply {
+            tvCharacterName.text = character.name
+            Glide.with(this.root).load(character.image).into(ivCharacterImage)
+        }
+    }
+
+    override fun getItemCount(): Int = characters.size
 }
