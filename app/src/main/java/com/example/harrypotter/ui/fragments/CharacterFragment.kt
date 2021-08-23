2212baf1acf5cf3085249aaad22cb8a267dd3a6c
Kagiri11 <charlesmaish423@gmail.com>
2021-08-23 23:59:55 +0300

    used glide

--- app/src/main/java/com/example/harrypotter/ui/fragments/CharacterFragment.kt
+++ app/src/main/java/com/example/harrypotter/ui/fragments/CharacterFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.harrypotter.R
+import com.example.harrypotter.databinding.FragmentCharacterBinding
+import com.example.harrypotter.databinding.FragmentHomeBinding
 
 class CharacterFragment : Fragment() {
+
+    private val args: CharacterFragmentArgs by navArgs()
+
+    private lateinit var binding: FragmentCharacterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_character, container, false)
+
+        val character = args.character
+        binding.apply {
+            characterAge.text=character.dateOfBirth
+            characterName.text=character.name
+            characterSpecies.text=character.species
+
+        }
+        Glide.with(requireActivity()).load(character.image).into(binding.characterImage)
+
+
+        return binding.root
     }
 }
