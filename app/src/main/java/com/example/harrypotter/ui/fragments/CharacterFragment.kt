c1ff61beaa94a2a3962b46b0f09da81049ad99d8
Kagiri11 <charlesmaish423@gmail.com>
2021-08-24 22:04:46 +0300

    edited character fragment

--- app/src/main/java/com/example/harrypotter/ui/fragments/CharacterFragment.kt
+++ app/src/main/java/com/example/harrypotter/ui/fragments/CharacterFragment.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.example.harrypotter.R
 import com.example.harrypotter.databinding.FragmentCharacterBinding
 import com.example.harrypotter.databinding.FragmentHomeBinding
+import java.util.*
 
 class CharacterFragment : Fragment() {
 
@@ -26,10 +27,16 @@ class CharacterFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_character, container, false)
 
         val character = args.character
+        fun getYearOfBirth(dob:String):Int{
+            return "${dob[dob.lastIndex-3]}${dob[dob.lastIndex-2]}${dob[dob.lastIndex-1]}${dob[dob.lastIndex]}".toInt()
+        }
+        val characterAgeNow= (2021-getYearOfBirth(character.dateOfBirth)).toString()
         binding.apply {
-            characterAge.text=character.dateOfBirth
+            characterAge.text=characterAgeNow
             characterName.text=character.name
             characterSpecies.text=character.species
+            characterEye.text= character.eyeColour
+            characterHair.text=character.house
 
         }
         Glide.with(requireActivity()).load(character.image).into(binding.characterImage)
